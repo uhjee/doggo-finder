583076ca33258ea1a28e0351b7bb2c75549627f1
ohmjeemin <ohmjeemin@gmail.com>
2021-09-21 23:51:03 +0900

    main, 폰트적용

---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// const Button = ({title}) => {
+//     return <button>{title}</button>;
+// }
+const Button = styled.button`
+    width: 7rem;
+    height: 3rem;
+    background: #EF7C8E;
+    border-radius: 12px;
+    font-weight: bold;
+    color: #ffffff;
+    font-family: 'SBAggroB';
+    font-size: 15px;
+`;
+
+export default Button;
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Button from './Button';
 
 const Home = () => {
     return (
         <div>
-            <p>안녕하세요 home이다</p>
+            <p>나와 잘 맞는 강아지 종류는 뭘까?</p>
+            <p>내 성격에 맞는 견종 찾기 테스트</p>
+            <p id="Logo">doggo finder</p>
+            <Button
+                onClick={() => alert("클릭")}
+            >시작하기
+            </Button>
             <Link to="/research">research로 이동</Link>
         </div>
     );

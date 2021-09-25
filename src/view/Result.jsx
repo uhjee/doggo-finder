9356f6a01c0698e9aa1bbad3614ee7966b35068a
ohmjeemin <ohmjeemin@bsidesoft.com>
2021-09-25 14:08:50 +0900

    distracted_type_sending

--- src/view/Result.jsx
+++ src/view/Result.jsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from 'react';
 import { isNil } from 'utils/commonUtil';
 import { RESULT_VIEWTYPE } from 'constant/stringEnum'
 
-const Result = () => {
+const Result = ({ type }) => {
+  console.log("result page type======",type)
   const [view, setView] = useState(RESULT_VIEWTYPE.PROGRESSIVE_BAR);
   return <>{view === 1 && <p>안녕하세요 result이다</p>}</>;
 };

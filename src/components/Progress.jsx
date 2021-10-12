8afb50d41d3d5eb8b3291a4d70da64240c809a94
ohmjeemin <ohmjeemin@gmail.com>
2021-10-12 23:28:43 +0900

    progress_bar_pic

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -4,6 +4,7 @@ export default function Progress({ rate }){
     const progressRate = `${rate}%`
     return (
         <div className={"progress"}>
+            <div style={{width:"20px", height:"20px", background:"pink", position:"absolute", right:"0"}}/>
             <div className={"progress painted"} style={{width:progressRate, display:"inline-block"}} />
             <div className={"dog"} style={{ display:"inline-block" }} />
         </div>

294622b96c8c97e6ca74bdf253243049bd39979f
ohmjeemin <ohmjeemin@gmail.com>
2021-10-14 21:50:28 +0900

    active_dog

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-export default function Progress({ rate }){
+export default function Progress({ rate, prev }){
     const progressRate = `${rate}%`
+    let dogStyle = ""
+    if(prev % 2 === 0) dogStyle = "dog";
+    else dogStyle = "dog2"
     return (
         <div className={"progress"}>
             <div className={"home"}/>
             <div className={"goal"}/>
             <div className={"progress painted"} style={{width:progressRate, display:"inline-block"}} />
-            <div className={"dog"} style={{ display:"inline-block" }} />
+            <div className={dogStyle} style={{ display:"inline-block" }} />
         </div>
     )
 }

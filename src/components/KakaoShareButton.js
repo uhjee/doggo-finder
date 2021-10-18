7852309811e79abc59e9b0dad067e8cf59d3b5b1
ohmjeemin <ohmjeemin@gmail.com>
2021-10-18 22:45:52 +0900

    share_button_css

--- src/components/KakaoShareButton.js
+++ src/components/KakaoShareButton.js
@@ -13,13 +13,14 @@ const KakaoShareButton = () => {
                 kakao.init(process.env.REACT_APP_KAKAO_KEY)
             }
             kakao.Link.createDefaultButton({
-                // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
+                // 해당 위치에 카카오링크 공유하기 버튼을 추가하고, 해당 버튼을 클릭했을 때 메세지 보내기를 요청
                 container: '#kakao-link-btn',
                 objectType: 'feed',
                 content: {
                     title: '스트리트 도고 파인더',
                     description: '내 성격과 가장 잘 맞는 강아지 종류는 뭘까? 나와 딱 맞는 강아지 찾기 테스트',
-                    imageUrl: 'IMAGE_URL', // i.e. process.env.FETCH_URL + /logo.png'
+                    imageUrl:'http://localhost:3000/public/logo512,png',
+                     // i.e. process.env.FETCH_URL + /logo.png'
                     link: {
                         mobileWebUrl: window.location.href,
                         webUrl: window.location.href,
@@ -38,12 +39,14 @@ const KakaoShareButton = () => {
         }
     }
     return (
-        <div className="kakao-share-button">
-             {/* Kakao share button */}
-                 <button id="kakao-link-btn">
-                 <img src="icons/kakao.png" alt="kakao-share-icon" />
-             </button>
-         </div>
+        <div>
+            <div style={{display:"flex",border:"1px solid #eaeaea",padding:"5px 8px",borderRadius:"8px",background:"#ffffff"}}>
+                 <div style={{fontSize:"10px",marginRight:"10px",display:"flex",alignItems:"center"}}>친구들과 공유하기</div>
+                 <div id="kakao-link-btn" >
+                    <img src="https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium.png" alt="kakao-share-icon" style={{width:"30px",height:"30px",display:"flex"}}/>
+                 </div>
+            </div>
+        </div>
     )
 }
 export default KakaoShareButton

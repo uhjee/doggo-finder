05638a124207fba2ed1a85b916f392d8583b8811
uhjee <oper1490@gmail.com>
2022-01-14 21:57:57 +0900

    image 개선                                   ✔ quote>   - image 파일들 public 폴더 하위로 이동 quote>   - background-image 에서 img 태그로 변경   - image 크기 압축

--- src/view/result/confirm.jsx
+++ src/view/result/confirm.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Button from 'components/Button';
-import successImage from 'assets/images/success_image.png';
 
 import { APP_STATE } from 'constant/stringEnum';
 
@@ -12,10 +11,15 @@ const Confirm = ({ setState }) => {
 
   return (
     <>
-      <div
+      {/* <div
         className="img_success"
         style={{ backgroundImage: `url(${successImage})` }}
-      ></div>
+      ></div> */}
+      <img
+        className="img_success"
+        src="img/success_img.png"
+        alt="success_img"
+      />
       <h3> 어떤 강아지가 찾아왔을까요? 🐶</h3>
       <Button text="결과 확인하기" handler={moveDescView} />
     </>
